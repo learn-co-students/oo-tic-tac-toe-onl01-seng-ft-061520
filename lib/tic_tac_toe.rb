96fb62bd3f534e6fc07f06d4cec70683080398f5
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-23 18:20:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,33 +25,64 @@
 
   end
 
-  def input_to_index(user)
-    user.to_i - 1
+  def input_to_index(user_input)
+    user_input.to_i - 1
     end
   end 
   
-  def move(board, index, symbol)
-    board[index] = symbol
+  def move(board, index, user)
+    board[index] = user
    
   end 
   
-  def position_taken?(board, index)
-    board[index] == "X" || board[index] == "O"
+#   def position_taken?(board, index)
+#     board[index] == "X" || board[index] == "O"
     
-  end 
+#   end 
   
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+#   def valid_move?(board, index)
+#     index.between?(0,8) && !position_taken?(board, index)
     
-  end 
+#   end 
   
-  def turn(board)
-    puts "Please enter 1-9"
-      userInput = gets.strip 
-      index = input_to_index(userInput)
-      if !valid_move?(board_index)
+#   def turn(board)
+#     puts "Please enter 1-9"
+#       userInput = gets.strip 
+#       index = input_to_index(userInput)
+#       if !valid_move?(board_index)
       
-      else
-        move(board, index, current_user(board))
-  end 
-end 
+#       else
+#         move(board, index, current_user(board))
+#   end 
+# end 
+
+#   def turn_count(board)
+#     count = 0
+#     board.each do |index|
+#       if index != " "
+#         count += 1 
+     
+#       end 
+#     end 
+#       count 
+      
+#     end 
+    
+#   def current_player(board)
+#     if turn_count(board) % 2 == 0 
+#       return "X"
+#     else 
+#       return "O"
+#     end
+    
+#   end 
+  
+#   def won?(board)
+#     win_conditions.each do |win|
+#       if win.all?{|y| board|y| == "X"}
+#         return win 
+#       elsif win.all?{|y| board|y| == "O"}
+#         return win 
+#       end 
+      
+#     end 
