fcc62351700a14d9b472f702864551b58469f41e
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-27 23:56:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class TicTacToe
   
   WIN_COMBINATIONS = [
@@ -31,7 +33,107 @@ class TicTacToe
     input.to_i - 1
   end
   
-  def move(board, token)
-     board.count{|token| token == "X" || token == "O"}
+  def move(space, token)
+    @board[space] = token
   end
+  
+  def position_taken?(index)
+    board[index] == "X" || board[index] == "O"
+  end
+  
+  def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(index)
+      true
+    else 
+      false
+    end 
+  end
+  
+  def turn
+    input = gets
+    user_input = input_to_index(input)
+    player = current_player
+    if valid_move?(user_input)
+      move(user_input, player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end 
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+    i_0 = win_combo[0]
+    i_1 = win_combo[1]
+    i_2 = win_combo[2]
+
+    position_1 = @board[i_0]
+    position_2 = @board[i_1]
+    position_3 = @board[i_2]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combo
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
+      end
+        false
+    end
+  end
+
+  
+  def full?
+    @board.all?{|taken| taken != " "}
+  end
+  
+  def draw?
+    !won? && full?
+  end
+  
+  
+  def over?
+    if won? || full? || draw?
+      true
+    else
+      false
+    end
+  end 
+  
+  def winner
+    WIN_COMBINATIONS.detect do |win_combo|
+    i_0 = win_combo[0]
+    i_1 = win_combo[1]
+    i_2 = win_combo[2]
+
+    position_1 = @board[i_0]
+    position_2 = @board[i_1]
+    position_3 = @board[i_2]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+      end
+        false
+    end
+  end
+  
+  def play 
+    until over? == true
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
